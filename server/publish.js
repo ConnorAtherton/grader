039a9fde31db9ba6e883aa7bf863b2f92205f488
digilord <dmorrigan@aeonstructure.com>
2013-12-23 08:10:57 -0700

    Changed example user code to only execute once on meteor startup.

--- server/publish.js
+++ server/publish.js
@@ -3,12 +3,15 @@ Meteor.publish('modules', function() {
   return Modules.find({user_id: this.userId});
 })
 
-var example_user = Meteor.users.findOne({username: 'example@example.com'}) || null;
-if(example_user == null) {
-  var options = {
-    username: 'example@example.com',
-    email: 'example@example.com',
-    password: 'example123'
-  };
-  Accounts.createUser(options)
-}
+Meteor.startup(function(){
+  var example_user = Meteor.users.findOne({username: 'example@example.com'}) || null;
+  if(example_user == null) {
+    console.log('Adding an example user.')
+    var options = {
+      username: 'example@example.com',
+      email: 'example@example.com',
+      password: 'example123'
+    };
+    Accounts.createUser(options)
+  }
+})
